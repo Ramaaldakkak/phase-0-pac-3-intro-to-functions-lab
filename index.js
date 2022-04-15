6a1c10cd52819ccc9b2cb378ba05d26cc8014a51
Rama Aldakkak <rama.aldakkak@gmail.com>
2022-04-15 15:23:39 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+//correct:
+
+function shout(string) {
+    return string.toUpperCase();;
+}
+
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+
+function sayHiToHeadphonedRoommate (string) {
+    let toRoommate;
+    if (string === string.toLowerCase()) {
+        toRoommate = "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+        toRoommate = "YES INDEED!";
+    } else if (string === "Let\'s have dinner together!") {
+        toRoommate = "I would love to!";
+    } 
+    return toRoommate;
+}
